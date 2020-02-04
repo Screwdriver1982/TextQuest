c38e45decb6fb3971eb95eb8505fd5a0d0283803
Screwdriver1982 <60478923+Screwdriver1982@users.noreply.github.com>
2020-02-04 21:18:48 +0300

    Initial commit

---
+++ Assets/TextGameQuest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextGameQuest : MonoBehaviour
+{
+    [Header("Elements")]
+    public Text titleTextVar;
+    public Text contentTextVar;
+
+    [Header("Config")]
+    [Tooltip("Game title")]public string title = "Game Title";
+    
+    [TextArea(minLines: 1, maxLines: 10)]public string content = "Content Text";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleTextVar.text = title;
+        contentTextVar.text = content;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
