0e17b32916ace26f5d7f57a6d2ffdff0da9db809
Screwdriver1982 <60478923+Screwdriver1982@users.noreply.github.com>
2020-02-20 20:49:04 +0300

    Scriptable Obj added

---
+++ Assets/Script/Data.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data File")] // любое имя для пункта меню
+public class Data : ScriptableObject  //особый тип объектов скриптов, которые нельзя привязывать к другим объектам, но можно использовать
+{
+    public string text;
+
+
+}
---
+++ Assets/Script/StepData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Step Data File")] //
+public class StepData : ScriptableObject
+{
+    public string title;
+    [TextArea(minLines: 10, maxLines: 20)] public string content;
+    public Sprite stepImage;
+    public StepData[] nextSteps;
+}
