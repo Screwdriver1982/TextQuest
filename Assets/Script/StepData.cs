6180a596e0f4384a847d2582b3ce52fc112c5510
Screwdriver1982 <60478923+Screwdriver1982@users.noreply.github.com>
2020-02-27 21:21:56 +0300

    comments

--- Assets/Script/StepData.cs
+++ Assets/Script/StepData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Step Data File")] //
+[CreateAssetMenu(menuName = "Step Data File")] //создает возможность в меню ассетов создать объект типа степ дата
 public class StepData : ScriptableObject
 {
     public string title;
